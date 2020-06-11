847a9879d4b6ffef6b79256e683c44d57ca0a929
Daniel Constantine <dconstantine86@gmail.com>
2020-06-11 15:55:29 +0000

    12/2/2020

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+    def home
+    end
+    
+    def about
+    end
+    
+    def faq
+    end
+    
+    def face
+    end
+    
+    def body
+    end
+    
+    def eyes
+    end
+    
+    def lips
+    end
+    
+    def nails
+    end
+    
+    def eye_brows
+    end
+    
+    def eye_liner
+    end
+    
+    def eye_shadow
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'faq', to: 'pages#faq'
+  get 'face', to: 'pages#face'
+  get 'body', to: 'pages#body'
+  get 'eyes', to: 'pages#eyes'
+  get 'lips', to: 'pages#lips'
+  get 'nails', to: 'pages#nails'
+  get 'eye_brows', to: 'pages#eye_brows'
+  get 'eye_liner', to: 'pages#eye_liner'
+  get 'eye_shadow', to: 'pages#eye_shadow'
+ resources :contacts, only: :create
+get 'contact_us', to: 'contacts#new'
 end
